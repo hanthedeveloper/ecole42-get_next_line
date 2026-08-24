5a2b178b8bb822d72089c6ee7836f88c40080ac3
han <bayramincel55@gmail.com>
2026-08-24 12:54:31 +0300

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -4,7 +4,7 @@
 /* verılen byte kadar oku ıcınde \n var mı bak
 varsa sag tarafı tut sol atrafı dondur */
 
-static char *give_line(char **str)
+static char *give_line(char **str) // ekrana yazdırılcak line
 {
 	unsigned int start;
 	char *line;
@@ -14,11 +14,22 @@ static char *give_line(char **str)
 	if (!temp)
 		return (NULL);
 	start = (temp - *str);
-	line = ft_substr(str, 0, start + 1); // malloc 2
-	*str = ft_substr(str, start + 1, ft_strlen(str) - 1 - start); // malloc 3
+	line = ft_substr(*str, 0, start + 1); // malloc 2
+	*str = ft_substr(*str, start + 1, ft_strlen(*str) - 1 - start); // malloc 3
 	if (!line)
 		return (NULL);
 	return (line);
+}
+
+static char *check(int i, char *str, char **hold)
+{
+	if (i = 0)
+	{
+		if (hold)
+		{
+			
+		}
+	}
 }
 
 char *get_next_line(int fd)
@@ -33,19 +44,18 @@ char *get_next_line(int fd)
 	str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1); // malloc 1
 	if (!str)
 		return (NULL);
+	// fd bufsize kontrolu + read okuyup str nın ıcıne atıcak yer acıyorum
+
 	i = 1;
 	while (i != 0)
 	{
 		i = read(fd, str, BUFFER_SIZE);
-		if (i == -1)
-		{
-			free(str);
-			return (NULL);
-		}
-		line = give_line(str);
-		if (!line)
-			return get_next_line(fd);
+		str[i] = '\0'; // c string yapıyorum
 		hold = ft_strjoin(hold, str); // malloc 4
+		check(i, str, &hold);
+		line = give_line(&hold);
+		if (line)
+			break;
 	}
 	free(str);
 	return (line);
