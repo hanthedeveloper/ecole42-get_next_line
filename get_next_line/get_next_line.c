3cb55df3778b9ac28407d6b964b16dff9ace7395
han <bayramincel55@gmail.com>
2026-08-24 12:05:16 +0300

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,13 +1,19 @@
+#include "get_next_line.h"
+#include <unistd.h>
+
+/* verılen byte kadar oku ıcınde \n var mı bak
+varsa sag tarafı tut sol atrafı dondur */
+
 static char *give_line(char **str)
 {
 	unsigned int start;
 	char *line;
 	char *temp;
 
-	temp = ft_strchr(str, '\n');
+	temp = ft_strchr(*str, '\n');
 	if (!temp)
 		return (NULL);
-	start = (temp - str);
+	start = (temp - *str);
 	line = ft_substr(str, 0, start + 1); // malloc 2
 	*str = ft_substr(str, start + 1, ft_strlen(str) - 1 - start); // malloc 3
 	if (!line)
@@ -22,12 +28,12 @@ char *get_next_line(int fd)
 	char *line;
 	static char *hold;
 
-	hold = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1); // malloc 1
 	if (!str)
 		return (NULL);
+	i = 1;
 	while (i != 0)
 	{
 		i = read(fd, str, BUFFER_SIZE);
@@ -37,7 +43,10 @@ char *get_next_line(int fd)
 			return (NULL);
 		}
 		line = give_line(str);
-		free(str);
+		if (!line)
+			return get_next_line(fd);
+		hold = ft_strjoin(hold, str); // malloc 4
 	}
+	free(str);
 	return (line);
 }
