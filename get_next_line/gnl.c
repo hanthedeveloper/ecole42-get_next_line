31e121034d376c9ab5e7b0f069830234b41fa8f6
han <bayramincel55@gmail.com>
2026-08-24 23:47:51 +0300

    Add files via upload

---
+++ get_next_line/gnl.c
@@ -0,0 +1,65 @@
+#include "gnl.h"
+#include "unistd.h"
+
+char *has_new_line(char **hold)
+{
+	char *line;
+	char *temp;
+	int linelen;
+	char *temph;
+
+	if (!*hold)
+		return (NULL);
+	temp = ft_strchr(*hold, '\n');
+	if (!temp)
+		return (NULL);
+	linelen = temp - *hold;
+	line = ft_substr(*hold, 0, linelen + 1);
+	temph = *hold;
+	*hold = ft_substr(*hold, linelen + 1, (ft_strlen(*hold) - linelen - 1));
+	free(temph);
+	return (line);
+}
+
+char *get_next_line(int fd)
+{
+	char *red;
+	static char *hold;
+	char *line;
+	char *temp;
+	int i;
+
+	red = (char *)malloc(BUFFER_SIZE + 1);
+	if (!red)
+		return (NULL);
+	i = 1;
+	while (i != 0)
+	{
+		i = read(fd, red, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free(red);
+			return (0);
+		}
+		red[i] = '\0';
+		temp = hold;
+		hold = ft_strjoin(hold, red);
+		free(temp);
+		line = has_new_line(&hold);
+		if (line)
+			break;
+		if (i == 0)
+		{
+			if (!hold)
+			{
+				free(red);
+				return (line);
+			}
+
+			line = hold;
+			hold = NULL;
+		}
+	}
+	free(red);
+	return (line);
+}
---
+++ get_next_line/utils.c
@@ -0,0 +1,76 @@
+#include "gnl.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t			i;
+	char			*ar;
+	size_t			slen;
+
+	if (!s)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (start >= slen || len == 0)
+		return (malloc(0));
+	if (len > slen - start)
+		len = slen - start;
+	ar = (char *)malloc(len + 1);
+	if (!ar)
+		return (NULL);
+	i = 0;
+	while (s[start] && (i < len))
+	{
+		ar[i] = s[start];
+		i++;
+		start++;
+	}
+	ar[i] = '\0';
+	return (ar);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((char)c == 0)
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ar;
+	size_t	i;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	ar = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!ar)
+		return (NULL);
+	i = 0;
+	while (s1 && i < ft_strlen(s1))
+		ar[i] = s1[i++];
+	i = 0;
+	while (s2 && i < ft_strlen(s2))
+		ar[ft_strlen(s1) + i] = s2[i++];
+	ar[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	return (ar);
+}
