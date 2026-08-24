7b5e258bbd44f4e7c7519e146abf6f11082c5478
han <bayramincel55@gmail.com>
2026-08-24 11:47:19 +0300

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,12 +1,5 @@
-#include "get_next_line.h"
-#include <unistd.h>
-
-/* verılen byte kadar oku ıcınde \n var mı bak
-varsa sag tarafı tut sol atrafı dondur */
-
-static char *give_line(char *str)
+static char *give_line(char **str)
 {
-	static char *tut;
 	unsigned int start;
 	char *line;
 	char *temp;
@@ -15,11 +8,9 @@ static char *give_line(char *str)
 	if (!temp)
 		return (NULL);
 	start = (temp - str);
-	line = ft_substr(str, 0, start + 1);
+	line = ft_substr(str, 0, start + 1); // malloc 2
+	*str = ft_substr(str, start + 1, ft_strlen(str) - 1 - start); // malloc 3
 	if (!line)
-		return (NULL);
-	tut = ft_substr(str, start + 1, (ft_strlen(str) - start - 1));
-	if (!tut)
 		return (NULL);
 	return (line);
 }
@@ -29,22 +20,24 @@ char *get_next_line(int fd)
 	ssize_t i;
 	char *str;
 	char *line;
+	static char *hold;
 
+	hold = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1); // malloc 1
 	if (!str)
 		return (NULL);
-	i = read(fd, str, BUFFER_SIZE);
-	if (i == -1)
+	while (i != 0)
 	{
+		i = read(fd, str, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free(str);
+			return (NULL);
+		}
+		line = give_line(str);
 		free(str);
-		return (NULL);
 	}
-	if (i == 0)
-		return (str);
-	
-	line = give_line(str);
-	free(str);
 	return (line);
 }
