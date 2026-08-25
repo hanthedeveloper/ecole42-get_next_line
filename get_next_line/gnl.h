24c6d90b0f14a07df118d14805927e20f57beb77
han <bayramincel55@gmail.com>
2026-08-25 08:52:27 +0300

    Update gnl.h

--- get_next_line/gnl.h
+++ get_next_line/gnl.h
@@ -11,5 +11,6 @@ char	*get_next_line(int fd);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
 
 #endif
