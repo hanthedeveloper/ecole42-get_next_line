033894140dbc7addf61ff228d01f4f1c0fbfc7ea
han <bayramincel55@gmail.com>
2026-08-25 09:12:03 +0300

    Update utils.c

--- get_next_line/utils.c
+++ get_next_line/utils.c
@@ -80,28 +80,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		s1 = "";
 	if (!s2)
 		s2 = "";
-	ar = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!ar)
-		return (NULL);
-	i = 0;
-	while (s1 && i < ft_strlen(s1))
-		ar[i] = s1[i++];
-	i = 0;
-	while (s2 && i < ft_strlen(s2))
-		ar[ft_strlen(s1) + i] = s2[i++];
-	ar[ft_strlen(s1) + ft_strlen(s2)] = '\0';
-	return (ar);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*ar;
-	size_t	i;
-
-	if (!s1)
-		s1 = "";
-	if (!s2)
-		s2 = "";
 	ar = (char *)malloc(s1len + ft_strlen(s2) + 1);
 	if (!ar)
 		return (NULL);
